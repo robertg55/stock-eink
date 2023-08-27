8cc694068b39940e61f58d2d409006c519e29834
robertg55 <robert55gh@gmail.com>
2023-08-26 23:22:31 -0400

    fix pythonpath

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 HOME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-export $PYTHONPATH=$HOME_DIR/stock-eink
+export PYTHONPATH=$HOME_DIR/stock-eink
 git -C $HOME_DIR/stock-eink reset --hard HEAD
 git -C $HOME_DIR/stock-eink pull
 python $HOME_DIR/stock-eink/stock_printer.py
