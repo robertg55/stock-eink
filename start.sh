d843dc3303d6e78e39d94ba4471c957dfe394300
robertg55 <robert55gh@gmail.com>
2023-08-26 23:17:47 -0400

    fix directory

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo $SCRIPT_DIR
-git -C $SCRIPT_DIR pull
-python $SCRIPT_DIR/stock_printer.py
+HOME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export $PYTHONPATH=$HOME_DIR/stock-eink
+git -C $HOME_DIR/stock-eink reset --hard HEAD
+git -C $HOME_DIR/stock-eink pull
+python $HOME_DIR/stock-eink/stock_printer.py
